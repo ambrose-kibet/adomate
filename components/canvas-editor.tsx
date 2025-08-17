c77ebaf9fd6967f7b46754f97ea013a8be49eb91
ambrose-kibet <ambrosekibet36@gmail.com>
2025-08-17 14:55:54 +0300

    fix: update Toolbar import path and enhance layout styling in CanvasEditor component

--- components/canvas-editor.tsx
+++ components/canvas-editor.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import FabricCanvas from "@/components/fabric-canvas";
-import Toolbar from "./toolbar";
+import Toolbar from "@/components/tools/toolbar";
+import { useLoadFonts } from "@/utils/font-loader";
 
 const CanvasEditor: React.FC = () => {
+  useLoadFonts();
   return (
-    <div className="flex flex-col items-center justify-center h-full w-full bg-accent">
+    <div className="flex flex-col items-center py-2 gap-2 justify-center h-full w-full bg-accent relative">
       <Toolbar />
       <FabricCanvas />
     </div>
