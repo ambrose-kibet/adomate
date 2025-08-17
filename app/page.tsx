91a78d607e99676c4b862a6c63e7d1644df15b31
ambrose-kibet <ambrosekibet36@gmail.com>
2025-08-17 14:55:33 +0300

    feat: add HistoryThumbnails component to Home layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import CanvasEditor from "@/components/canvas-editor";
+import HistoryThumbnails from "@/components/history-thumbnails";
 import Sidebar from "@/components/sidebar";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-cols-[400px_1fr]  min-h-[calc(100vh-73px)] ">
+    <div className="font-sans grid grid-cols-[400px_1fr_120px]  min-h-[calc(100vh-73px)] ">
       <Sidebar />
       <CanvasEditor />
+      <HistoryThumbnails />
     </div>
   );
 }
