141195c28ce29cd4d06a06dcc4c19000e96b7f74
ambrose-kibet <ambrosekibet36@gmail.com>
2025-08-17 14:55:38 +0300

    fix: add missing newline for improved code readability in providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "@/lib/redux/store";
+
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <ThemeProvider
